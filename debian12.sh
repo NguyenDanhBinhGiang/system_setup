eb7c47a89e269c37673682b78de4deb9255e8d93
NguyenDanhBinhGiang <nguyendanhbinhgiang@gmail.com>
2025-12-11 05:41:29 +0700

    update debian scripts

---
+++ debian12.sh
@@ -0,0 +1,178 @@
+if [ $# -gt 0 ]
+then
+  if [[ "$1" == "-y" ]];
+  then
+    ok_all=1;
+  else
+    ok_all=0;
+  fi;
+fi;
+
+# todo: check config file existed, delete them if needed
+# todo: fix /etc/apt/source.list
+#  add contrib non-free and delete cd-rom
+#  delete this line: deb cdrom:[Debian GNU/Linux 12.6.0 _Bookworm_ - Official amd64 DVD Binary-1 with firmware 20240629-10:19]/ bookworm contrib main non-free-firmware
+
+
+echo "This script is written for Debian 12 Bookworm with Gnome.
+If you are using other version, check the script before run it" && \
+if [[ $ok_all != 1 ]];
+then
+  read -p "Are you ready to run this script? (Y/N)" confirm
+  if [[ "$confirm" != "Y" && "$confirm" != "y" ]];
+  then exit 1;
+  fi;
+fi;
+
+echo "deb http://deb.debian.org/debian bookworm contrib main non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
+
+# Install packages
+sudo apt-get update && sudo apt-get -y install \
+git curl wget bash-completion \
+python3-pip python3-dev python3-venv \
+dconf-editor gnome-shell-extension-dash-to-panel \
+gnome-shell-extension-desktop-icons-ng software-properties-common \
+gparted grub-customizer timeshift vlc fonts-unifont
+
+
+# remove unwanted gnome packages
+sudo apt purge --auto-remove gnome-games
+# tweak gnome
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
+
+# remove Firefox ESR and install new version
+sudo apt purge --autoremove -y firefox* && \
+sudo install -d -m 0755 /etc/apt/keyrings && \
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null && \
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}' && \
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null && \
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla  && \
+sudo apt-get update && sudo apt-get install -y firefox
+
+
+# Install Flatpak
+sudo apt install -y flatpak && \
+sudo apt install -y gnome-software-plugin-flatpak && \
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+
+# install docker
+# Add Docker's official GPG key:
+sudo apt-get update && \
+sudo apt-get install ca-certificates curl && \
+sudo install -m 0755 -d /etc/apt/keyrings && \
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && \
+sudo chmod a+r /etc/apt/keyrings/docker.asc && \
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+sudo apt-get update && \
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \
+# add current user to docker group
+# sudo groupadd docker ;
+sudo usermod -aG docker $USER && \
+newgrp docker;
+
+
+# install steam
+mkdir /tmp/steam;
+sudo wget "https://cdn.akamai.steamstatic.com/client/installer/steam.deb" -O /tmp/steam/steam.deb && \
+if sudo dpkg -i /tmp/steam/steam.deb
+then echo "ok";
+else sudo apt-get -yf install && sudo dpkg -i /tmp/steam/steam.deb
+fi;
+
+
+# install discord
+mkdir /tmp/discord;
+sudo wget "https://discord.com/api/download?platform=linux&format=deb" -O /tmp/discord/discord.deb && \
+if sudo dpkg -i /tmp/discord/discord.deb
+then echo "ok";
+else sudo apt-get -yf install && sudo dpkg -i /tmp/discord/discord.deb
+fi;
+
+
+# install convenient scripts
+git clone https://github.com/NguyenDanhBinhGiang/convenient_scripts.git ~/script &&\
+sudo cp ~/script/docker_prune /usr/local/bin/ && \
+mkdir ~/.bash_completion.d && \
+sudo wget "https://raw.githubusercontent.com/cykerway/complete-alias/master/complete_alias" -O ~/.bash_completion.d/complete_alias && \
+git clone https://github.com/nvbn/thefuck.git /tmp/thefuck &&\
+pip3 install --user /tmp/thefuck --break-system-packages && \
+cat ~/script/.bashrc > ~/.bashrc
+
+
+# ----------- Install EasyEffects ----------
+# apt install --no-install-recommends xdg-desktop-portal-gnome -y
+# flatpak install flathub com.github.wwmm.easyeffects
+# flatpak permission-reset com.github.wwmm.easyeffects
+sudo apt install -y easyeffects
+
+
+# install spoof-dpi
+curl -fsSL https://raw.githubusercontent.com/xvzc/SpoofDPI/main/install.sh | bash -s linux-amd64 && \
+echo "[Unit]
+Description=Spoof DPI
+
+[Service]
+User=hiragawa
+WorkingDirectory=/home/$USER/.spoofdpi/bin/
+ExecStart=/home/$USER/.spoofdpi/bin/spoofdpi -port 8123
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/spoof-dpi.service && \
+sudo systemctl daemon-reload && \
+sudo systemctl enable spoof-dpi.service && \
+sudo systemctl start spoof-dpi.service && \
+# prompt to automatically settup proxy server
+printf "\n\n\n\nSpoof-DPI proxy server installed at 127.0.0.1:8123.\n
+Setup your proxy server in network setting if you want to use Spoof-DPI" && \
+read -p "Automatically setup proxy? Y/N" confirm && \
+if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+then echo "
+http_proxy="http://127.0.0.1:8123/"
+https_proxy="http://127.0.0.1:8123/"
+ftp_proxy="http://127.0.0.1:8123/"
+export http_proxy ftp_proxy https_proxy
+" | sudo tee -a /etc/profile;
+fi;
+
+
+# install java
+read -p "Install Java? (Y/N)" confirm;
+if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+then
+  mkdir /tmp/java;
+  sudo wget "https://download.oracle.com/java/17/archive/jdk-17.0.11_linux-x64_bin.deb" -O /tmp/java/jdk17.deb && \
+  if sudo dpkg -i /tmp/java/jdk17.deb
+  then echo "ok";
+  else sudo apt-get -yf install && sudo dpkg -i /tmp/java/jdk17.deb
+  fi;
+fi;
+
+
+# install nvidia driver
+printf "\n\nMake sure you have fixed /etc/apt/source.list before this step.\n"
+read -p "Ready?" confirm;
+if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+then
+sudo apt install -y nvidia-driver firmware-misc-nonfree;
+fi;
+
+
+# Complete notify
+printf "\n\nInstallation finished. Please restart your computer.\n"
+# prompt for reboot
+read -p "Restart now? (Y/N)" confirm;
+if [[ "$confirm" != "Y" && "$confirm" != "y" || $ok_all == 1 ]];
+then exit 1;
+fi;
+sudo reboot
---
+++ debian13.sh
@@ -0,0 +1,187 @@
+if [ $# -gt 0 ]
+then
+  if [[ "$1" == "-y" ]];
+  then
+    ok_all=1;
+  else
+    ok_all=0;
+  fi;
+fi;
+
+echo "This script is written for Debian 12 Bookworm with Gnome.
+If you are using other version, check the script before run it" && \
+if [[ $ok_all != 1 ]];
+then
+  read -p "Are you ready to run this script? (Y/N)" confirm
+  if [[ "$confirm" != "Y" && "$confirm" != "y" ]];
+  then exit 1;
+  fi;
+fi;
+
+# add contrib and non-free
+echo "deb http://deb.debian.org/debian trixie contrib main non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
+# install linux header
+sudo apt update && sudo apt install -y linux-headers-$(dpkg --print-architecture)
+
+
+# Install packages
+sudo apt-get update && sudo apt-get -y install \
+git curl wget bash-completion \
+python3-pip python3-dev python3-venv \
+dconf-editor gparted grub-customizer backintime-qt psensor vlc fonts-unifont
+# TODO: software-properties-common gnome-shell-extension-desktop-icons-ng gnome-shell-extension-dash-to-panel 
+# https://extensions.gnome.org/extension/615/appindicator-support/
+
+
+# remove unwanted gnome packages
+# tweak gnome
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+gsettings set org.gnome.nautilus.preferences default-sort-order 'type'
+
+
+# remove Firefox ESR and install new version
+sudo apt purge --autoremove -y firefox* && \
+sudo install -d -m 0755 /etc/apt/keyrings && \
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null && \
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}' && \
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null && \
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla  && \
+sudo apt-get update && sudo apt-get install -y firefox
+
+
+# Install Flatpak
+sudo apt install -y flatpak && \
+sudo apt install -y gnome-software-plugin-flatpak && \
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+
+# install docker
+# Add Docker's official GPG key:
+sudo apt-get update && \
+sudo apt-get install ca-certificates curl && \
+sudo install -m 0755 -d /etc/apt/keyrings && \
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && \
+sudo chmod a+r /etc/apt/keyrings/docker.asc && \
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+sudo apt-get update && \
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \
+# add current user to docker group
+# sudo groupadd docker ;
+sudo usermod -aG docker $USER && \
+newgrp docker;
+
+
+# install steam
+mkdir /tmp/steam;
+sudo wget "https://cdn.akamai.steamstatic.com/client/installer/steam.deb" -O /tmp/steam/steam.deb && \
+if sudo dpkg -i /tmp/steam/steam.deb
+then echo "ok";
+else sudo apt-get -yf install && sudo dpkg -i /tmp/steam/steam.deb
+fi;
+
+
+# install discord
+mkdir /tmp/discord;
+sudo wget "https://discord.com/api/download?platform=linux&format=deb" -O /tmp/discord/discord.deb && \
+if sudo dpkg -i /tmp/discord/discord.deb
+then echo "ok";
+else sudo apt-get -yf install && sudo dpkg -i /tmp/discord/discord.deb
+fi;
+
+
+# install convenient scripts
+git clone https://github.com/NguyenDanhBinhGiang/convenient_scripts.git ~/script &&\
+sudo cp ~/script/docker_prune /usr/local/bin/ && \
+mkdir ~/.bash_completion.d && \
+sudo wget "https://raw.githubusercontent.com/cykerway/complete-alias/master/complete_alias" -O ~/.bash_completion.d/complete_alias && \
+cat ~/script/.bashrc > ~/.bashrc
+# git clone https://github.com/nvbn/thefuck.git /tmp/thefuck &&\
+# pip3 install --user /tmp/thefuck --break-system-packages && \
+
+
+# ----------- Install EasyEffects ----------
+# apt install --no-install-recommends xdg-desktop-portal-gnome -y
+# flatpak install -y flathub com.github.wwmm.easyeffects
+# flatpak permission-reset com.github.wwmm.easyeffects
+sudo apt install -y easyeffects
+
+
+# install spoof-dpi
+read -p "Install Spoof DPI? (Y/N)" confirm;
+if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+then
+  curl -fsSL https://raw.githubusercontent.com/xvzc/SpoofDPI/main/install.sh | bash -s linux-amd64 && \
+  echo "[Unit]
+Description=Spoof DPI
+
+[Service]
+User=hiragawa
+WorkingDirectory=/home/$USER/.spoofdpi/bin/
+ExecStart=/home/$USER/.spoofdpi/bin/spoofdpi -port 8123
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/spoof-dpi.service && \
+  sudo systemctl daemon-reload && \
+  sudo systemctl enable spoof-dpi.service && \
+  sudo systemctl start spoof-dpi.service && \
+  # prompt to automatically settup proxy server
+  printf "\n\n\n\nSpoof-DPI proxy server installed at 127.0.0.1:8123.\nSetup your proxy server in network setting if you want to use Spoof-DPI" && \
+  read -p "Automatically setup proxy? Y/N" confirm && \
+  if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+  then echo "
+http_proxy="http://127.0.0.1:8123/"
+https_proxy="http://127.0.0.1:8123/"
+ftp_proxy="http://127.0.0.1:8123/"
+export http_proxy ftp_proxy https_proxy
+" | sudo tee -a /etc/profile;
+  fi;
+fi;
+
+# install java
+read -p "Install Java? (Y/N)" confirm;
+if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+then
+  mkdir /tmp/java;
+  sudo wget "https://download.oracle.com/java/17/archive/jdk-17.0.11_linux-x64_bin.deb" -O /tmp/java/jdk17.deb && \
+  if sudo dpkg -i /tmp/java/jdk17.deb
+  then echo "ok";
+  else sudo apt-get -yf install && sudo dpkg -i /tmp/java/jdk17.deb
+  fi;
+fi;
+
+
+# install nvidia driver
+printf "\n\nMake sure you have fixed /etc/apt/source.list before this step.\n"
+read -p "Ready? (Y/N)" confirm;
+if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+then
+  sudo apt install -y nvidia-kernel-dkms nvidia-driver firmware-misc-nonfree;
+  printf "\n\n\n\n\n\nDKMS ${RED}check${NC} result:\n\n";
+  sudo dkms status;
+  read -p "Enable Wayland? (Y/N)" confirm;
+  if [[ "$confirm" == "Y" || "$confirm" == "y" || $ok_all == 1 ]]
+  then
+    echo 'options nvidia NVreg_PreserveVideoMemoryAllocations=1' | sudo tee -a /etc/modprobe.d/nvidia-power-management.conf
+    echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf
+    echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1" | sudo tee -a /etc/modprobe.d/nvidia-options.conf
+  fi;
+fi;
+
+
+# Complete notify
+printf "\n\nInstallation finished. Please restart your computer.\n"
+# prompt for reboot
+read -p "Restart now? (Y/N)" confirm;
+if [[ "$confirm" != "Y" && "$confirm" != "y" || $ok_all == 1 ]];
+then exit 1;
+fi;
+sudo reboot
